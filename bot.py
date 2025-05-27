1906e0abccba1efd9d9caa922c1f7443c1714905
Shaun Vivian Dsouza <shaunvividsz@gmail.com>
2025-05-27 16:56:35 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -253,7 +253,7 @@ def trade_symbol(symbol, per_trade_usdt, base_asset="USDT"):
         df = fetch_ohlcv(symbol)
         df = apply_indicators(df)
         should_buy_flag,passed_reasons,skip_reasons=should_buy(df)
-        if should_buy(df) and confirm_higher_timeframe(symbol):
+        if should_buy_flag and confirm_higher_timeframe(symbol):
             log_successful_buy(symbol,passed_reasons)
             precision = get_precision(symbol)
             amount = round(per_trade_usdt / df["close"].iloc[-1], precision)
